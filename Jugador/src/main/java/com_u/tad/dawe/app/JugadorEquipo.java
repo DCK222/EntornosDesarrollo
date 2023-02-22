0c3911dd28a8323705af2670aea1dc6864f9cd39
DCK222 <alegard22@gmail.com>
2023-02-22 17:15:09 +0100

    Alejandro

--- Jugador/src/main/java/com_u/tad/dawe/app/JugadorEquipo.java
+++ Jugador/src/main/java/com_u/tad/dawe/app/JugadorEquipo.java
@@ -1,18 +1,44 @@
 package com_u.tad.dawe.app;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import com_utad.dawe.model.JugarBalonmano;
 
 public class JugadorEquipo {
 	
 	public static void main (String[] args) {
 		
-		JugarBalonmano jugador1 = new JugarBalonmano("alfre" , 20);
 		
-		JugarBalonmano jugador2 = new JugarBalonmano("Luis" , 18);
+		ArrayList <JugarBalonmano>  Jugadores = new ArrayList <JugarBalonmano>();
 		
-		for (int i = 0; i < args.length; i++) {
+		for (int i = 1; i < 3; i++) {
+			Scanner sc = new Scanner  (System.in);
+			System.out.println("Como se llama el jugador " + i + "?");
+			//sc.nextLine();
+			String nombre = sc.nextLine();
+		
+			Scanner sc2 = new Scanner  (System.in);
+			System.out.println("Cual es la dorsal del jugador " + i + "?");
+			int dorsal = sc2.nextInt();
 			
+			int anotacion = (int)(Math.random()*20);
+			System.out.println("Anotaciones = " + anotacion);
+
+			int bloqueo = (int)(Math.random()*30);
+			System.out.println("Bloqueos = " + bloqueo);
+			
+			int TiempoJugado = (int) System.currentTimeMillis();
+			System.out.println("Tiempo jugado = " + TiempoJugado + " Segs");
+			
+			JugarBalonmano datos = new JugarBalonmano (nombre , dorsal);
+			datos.setAnotacion(anotacion);
+			datos.setBloqueo(bloqueo);
+			datos.setTiempoJugado(TiempoJugado);
+			Jugadores.add(datos);
+			System.out.println(datos.toString());
 		}
+		
 		
 		
 		
